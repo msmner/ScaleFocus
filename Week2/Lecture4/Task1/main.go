ce26ad82ea32a99c6858c3e5095fbf200d26544c
Ivaylo-Nikolov <ivaylo.nikolov@mentormate.com>
2022-02-23 18:30:53 +0200

    fix error handling

--- Week2/Lecture4/Task1/main.go
+++ Week2/Lecture4/Task1/main.go
@@ -18,15 +18,23 @@ func main() {
 			continue
 		}
 
-		year, month := processInput(input)
+		year, month, err := processInput(input)
+		if err != nil {
+			fmt.Printf("Got Error %s: ", err)
+			continue
+		}
 
-		daysInMonth, isLeapYear := daysInMonth(year, month)
+		daysInMonth, isLeapYear, err := daysInMonth(year, month)
+		if err != nil {
+			fmt.Printf("Got Error %s: ", err)
+			continue
+		}
 
 		fmt.Printf("Days in month are %d. Year is leap year: %v\n", daysInMonth, isLeapYear)
 	}
 }
 
-func processInput(args string) (int, int) {
+func processInput(args string) (int, int, error) {
 	inputArr := strings.Split(args, " ")
 	for i := range inputArr {
 		inputArr[i] = strings.TrimSpace(inputArr[i])
@@ -34,39 +42,41 @@ func processInput(args string) (int, int) {
 
 	month, err := strconv.Atoi(inputArr[0])
 	if err != nil {
-		fmt.Printf("Error parsing month %v: %s", month, err)
+		formattedError := fmt.Errorf("error parsing month %v: %s", month, err)
+		return 0, 0, formattedError
 	}
 
 	year, err := strconv.Atoi(inputArr[1])
 	if err != nil {
-		fmt.Printf("Error parsing year %v: %s", year, err)
+		formattedError := fmt.Errorf("error parsing year %v: %s", year, err)
+		return 0, 0, formattedError
 	}
 
-	return month, year
+	return month, year, nil
 }
 
-func daysInMonth(month int, year int) (int, bool) {
+func daysInMonth(month int, year int) (int, bool, error) {
 	if month < 1 || month > 12 {
-		fmt.Printf("Invalid month %d", month)
-		return 0, false
+		formattedError := fmt.Errorf("invalid month %d", month)
+		return 0, false, formattedError
 	}
 
 	isLeapYear := checkIfLeapYear(year)
 
 	switch month {
 	case 1, 3, 5, 7, 8, 10, 12:
-		return 31, isLeapYear
+		return 31, isLeapYear, nil
 	case 4, 6, 9, 11:
-		return 30, isLeapYear
+		return 30, isLeapYear, nil
 	case 2:
 		if isLeapYear {
-			return 29, isLeapYear
+			return 29, isLeapYear, nil
 		} else {
-			return 28, isLeapYear
+			return 28, isLeapYear, nil
 		}
 	}
 
-	return 0, false
+	return 0, false, nil
 }
 
 func checkIfLeapYear(year int) bool {
