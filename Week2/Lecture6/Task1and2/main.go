e7c1e3a6f423e5575f8daa75e2711ec860b10b13
Ivaylo-Nikolov <ivaylo.nikolov@mentormate.com>
2022-02-27 16:58:39 +0200

    Lecture6Tasks

---
+++ Week2/Lecture6/Task1and2/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+	magicList := MagicList{}
+	add(&magicList, 10)
+	add(&magicList, 12)
+	add(&magicList, 14)
+	result := toSlice(&magicList)
+	fmt.Println(result)
+}
+
+type Item struct {
+	Value    int
+	PrevItem *Item
+}
+
+type MagicList struct {
+	LastItem *Item
+}
+
+func add(l *MagicList, value int) {
+	if l.LastItem == nil {
+		item := &Item{value, nil}
+		l.LastItem = item
+	} else {
+		item := &Item{value, l.LastItem}
+		l.LastItem = item
+	}
+}
+
+func toSlice(l *MagicList) []int {
+	result := []int{}
+	for {
+		if l.LastItem.PrevItem == nil {
+			return append(result, l.LastItem.Value)
+		} else {
+			result = append(result, l.LastItem.Value)
+			l.LastItem = l.LastItem.PrevItem
+		}
+	}
+}
